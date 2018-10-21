ec93e7000014c1f4a7c548806f77329e1ab6344f
sigimund <33035287+sigimund@users.noreply.github.com>
2018-10-21 19:40:22 +0200

    Prvi teden
    Popolni začetek pri projektu

---
+++ app/src/main/java/org/um/ziga/lovznamenitosti/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.um.ziga.lovznamenitosti
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
