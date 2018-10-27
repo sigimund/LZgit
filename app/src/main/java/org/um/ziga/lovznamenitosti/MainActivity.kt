2bda57b34c3c748ac209980ce0c21e5452dd1cf1
sigimund <33035287+sigimund@users.noreply.github.com>
2018-10-27 15:06:29 +0200

    Delo

--- app/src/main/java/org/um/ziga/lovznamenitosti/MainActivity.kt
+++ app/src/main/java/org/um/ziga/lovznamenitosti/MainActivity.kt
@@ -2,14 +2,22 @@ package org.um.ziga.lovznamenitosti
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText;
 
 class MainActivity : AppCompatActivity() {
 
     private EditText upIme;
+    private EditText upGeslo;
+    private Button gumb;
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main);
+
+        upIme = (EditText)findViewById(R.id.upIme);
+        upGeslo = (EditText)findViewById(R.id.upGeslo);
+        gumb = (Button)findViewById(R.id.gumb);
     }
 }
