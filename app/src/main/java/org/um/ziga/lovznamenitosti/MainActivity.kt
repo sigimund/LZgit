b11f07508ad97aca91f4819a3317787c5cd1df90
sigimund <33035287+sigimund@users.noreply.github.com>
2018-10-27 14:52:49 +0200

    Delo1

--- app/src/main/java/org/um/ziga/lovznamenitosti/MainActivity.kt
+++ app/src/main/java/org/um/ziga/lovznamenitosti/MainActivity.kt
@@ -2,8 +2,11 @@ package org.um.ziga.lovznamenitosti
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText;
 
 class MainActivity : AppCompatActivity() {
+
+    private EditText upIme;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
